4eda8aac5bd304de9ddff1e545e39dabdb736912
wiwilin <katherine.linw@gmail.com>
2020-07-19 23:55:52 +1000

    Updates

---
+++ precache-manifest.d13806673580fc3816f6c5aa25d84ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9541087f72344fd62b3e3313c70152cc",
+    "url": "/COVID19_amid_twitter_analysing/index.html"
+  },
+  {
+    "revision": "224bceca71a89bef023c",
+    "url": "/COVID19_amid_twitter_analysing/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "6567cd43404755653844",
+    "url": "/COVID19_amid_twitter_analysing/static/css/main.9feadecf.chunk.css"
+  },
+  {
+    "revision": "224bceca71a89bef023c",
+    "url": "/COVID19_amid_twitter_analysing/static/js/2.721965ab.chunk.js"
+  },
+  {
+    "revision": "257c8c5a628c6334d25a089c51fcffa1",
+    "url": "/COVID19_amid_twitter_analysing/static/js/2.721965ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6567cd43404755653844",
+    "url": "/COVID19_amid_twitter_analysing/static/js/main.7f7e9e4c.chunk.js"
+  },
+  {
+    "revision": "d741e023223303d4043d",
+    "url": "/COVID19_amid_twitter_analysing/static/js/runtime-main.1e8094e5.js"
+  }
+]);
